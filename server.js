1fb8715b89dd57430ea7d5ed6499a4adbe74cdfa
R3D1NM <r3dinm@gmail.com>
2024-02-14 22:29:12 +0900

    [build] connect database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require("dotenv").config();
 
 // Constanst
 const PORT = 5000;
@@ -12,6 +13,11 @@ app.use(express.json())
 // Routes
 const productRoutes = require('./routes')
 app.use("/api/products",productRoutes)
+
+// Database
+const mongoose = require('mongoose')
+const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@movieweb.nvzwk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+mongoose.connect(dbURI).then(() => console.log('[DATABASE] CONNECTED')).catch(() => console.log('[DATABASE] CONNECTION FAILED'))
 
 
 app.listen(PORT)
